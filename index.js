a086159f3513dd08581facc55d3e5c036f28ae5b
Audu Godwin DavizXcoded <33580206+DavizXcoded@users.noreply.github.com>
2019-10-13 15:33:46 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,43 @@
-   //This is my first Javascript Code
-   console.log('Hello World');
+
+//console.log('Hello World');
+
+
+//let InterestRate = 1;
+//InterestRate = 1;
+//console.log(InterestRate);
+
+//let name = 'Godwin'; //This is a string
+//let age = 31; //This is a number
+//let isApproved = true; //This is a Boolean Literal
+//let firstName = undefined; 
+//let lastName = null;
+
+// let Person = {
+//     name: 'Godwin',
+//     age: 20
+// };
+
+// Dot Notation
+// Person.name = 'David';
+
+// //Bracket Notation
+// Person ['name'] = 'Mary'
+
+// console.log(Person.name);
+
+// let selectedColors = ['red', 'blue'];
+// selectedColors[2] = 'green';
+// console.log(selectedColors.length);
+
+// Performing a task
+function greet (name, lastName) {
+    console.log('Hello ' + name + ' ' + lastName);
+}
+
+// Calculating a task
+function square (number){
+    return number * number;
+}
+
+console.log(square(2));
+//greet('John', 'David');
